c6c5a1ad2f0210a73e2d7e3b5afb5629d41c88cb
Adhul Krishna A S <adhulsudhi@gmail.com>
2026-06-30 22:36:14 +0100

    Created simple header and restaurant card component

--- App1.js
+++ App1.js
@@ -9,11 +9,10 @@
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
+  React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "Hello from nested react"),
-  ),
+    React.createElement("h2", {}, "Hello from h2"),
+  ]),
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
